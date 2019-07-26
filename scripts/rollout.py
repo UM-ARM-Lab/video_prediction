ac92a2875e006994f973431ee78722e44766318e
Peter Mitrano <pmitrano@umich.edu>
2019-07-26 18:05:48 -0400

    rewriting rollout for use in CEM

--- scripts/rollout.py
+++ scripts/rollout.py
@@ -4,6 +4,7 @@ from __future__ import division
 from __future__ import print_function
 
 import argparse
+import numpy as np
 import matplotlib.pyplot as plt
 import errno
 from collections import OrderedDict
@@ -13,8 +14,6 @@ import json
 import os
 import random
 
-import cv2
-import numpy as np
 import tensorflow as tf
 
 from video_prediction import models
@@ -44,61 +43,9 @@ def main():
         np.random.seed(args.seed)
         random.seed(args.seed)
 
-    model_hparams_dict = {}
-    checkpoint_dir = os.path.normpath(args.checkpoint)
-    if not os.path.isdir(args.checkpoint):
-        checkpoint_dir, _ = os.path.split(checkpoint_dir)
-    if not os.path.exists(checkpoint_dir):
-        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), checkpoint_dir)
-    with open(os.path.join(checkpoint_dir, "options.json")) as f:
-        print("loading options from checkpoint %s" % args.checkpoint)
-        options = json.loads(f.read())
-        args.model = args.model or options['model']
-    try:
-        with open(os.path.join(checkpoint_dir, "model_hparams.json")) as f:
-            model_hparams_dict = json.loads(f.read())
-    except FileNotFoundError:
-        print("model_hparams.json was not loaded because it does not exist")
+    input_phs, inputs, sequence_length = build_dataset(args, context_frames)
 
-    # all arrays must be the length of the entire prediction, but only the first two will be used
-    actions = np.fromstring(args.actions, dtype=np.float32, sep=',').reshape([1, -1, 2])
-    sequence_length = actions.shape[1]
-    states = np.zeros([1, sequence_length, 2], np.float32)
-    image_bytes = np.zeros([1, sequence_length, 64, 64, 3], np.float32)
-    for time_step_idx in range(context_frames):
-        states[0, time_step_idx] = np.fromstring(args.states[time_step_idx], dtype=np.float32, sep=',')
-        rgba_image_uint8 = np.array(Image.open(args.images[time_step_idx]), dtype=np.uint8)
-        rgb_image_float = rgba_image_uint8[:, :, :3].astype(np.float32) / 255.0
-        image_bytes[0, time_step_idx] = rgb_image_float
-
-    VideoPredictionModel = models.get_model_class(args.model)
-    hparams_dict = dict(model_hparams_dict)
-    hparams_dict.update({
-        'context_frames': context_frames,
-        'sequence_length': sequence_length,
-    })
-    model = VideoPredictionModel(
-        mode='test',
-        hparams_dict=hparams_dict,
-        hparams=args.model_hparams)
-
-    sequence_length = model.hparams.sequence_length
-    context_frames = model.hparams.context_frames
-    future_length = sequence_length - context_frames
-
-    dataset = tf.data.Dataset.from_tensor_slices((OrderedDict({
-        'images': image_bytes,
-        'states': states,
-        'actions': actions,
-    })))
-    batched_dataset = dataset.batch(1, drop_remainder=True)
-
-    iterator = batched_dataset.make_one_shot_iterator()
-    inputs = iterator.get_next()
-    input_phs = {k: tf.placeholder(v.dtype, v.shape, '%s_ph' % k) for k, v in inputs.items()}
-
-    with tf.variable_scope(''):
-        model.build_graph(input_phs)
+    model = build_model(args, context_frames, input_phs, sequence_length)
 
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.1)
     config = tf.ConfigProto(gpu_options=gpu_options, allow_soft_placement=True)
@@ -115,48 +62,100 @@ def main():
         'states': model.outputs['gen_states'],
     })
     results = sess.run(fetches, feed_dict=feed_dict)
-    gen_images = results['images']
-    gen_states = results['states']
-    print(states)
-    print(gen_states)
 
-    plt.scatter(states[0, :context_frames, 0], states[0, :context_frames, 1], label='input_states')
-    # plt.scatter(gen_states[:], label='gen_states')
+    visualize(input_results, results, context_frames, args)
+
+
+def build_model(args, context_frames, input_phs, sequence_length):
+    model_hparams_dict = {}
+    checkpoint_dir = os.path.normpath(args.checkpoint)
+    if not os.path.isdir(args.checkpoint):
+        checkpoint_dir, _ = os.path.split(checkpoint_dir)
+    if not os.path.exists(checkpoint_dir):
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), checkpoint_dir)
+    with open(os.path.join(checkpoint_dir, "options.json")) as f:
+        print("loading options from checkpoint %s" % args.checkpoint)
+        options = json.loads(f.read())
+        args.model = args.model or options['model']
+    try:
+        with open(os.path.join(checkpoint_dir, "model_hparams.json")) as f:
+            model_hparams_dict = json.loads(f.read())
+    except FileNotFoundError:
+        print("model_hparams.json was not loaded because it does not exist")
+
+    VideoPredictionModel = models.get_model_class(args.model)
+    hparams_dict = dict(model_hparams_dict)
+    hparams_dict.update({
+        'context_frames': context_frames,
+        'sequence_length': sequence_length,
+    })
+    model = VideoPredictionModel(
+        mode='test',
+        hparams_dict=hparams_dict,
+        hparams=args.model_hparams)
+    with tf.variable_scope(''):
+        model.build_graph(input_phs)
+    return model
+
+
+def build_dataset(args, context_frames):
+    # all arrays must be the length of the entire prediction, but only the first two will be used
+    actions = np.fromstring(args.actions, dtype=np.float32, sep=',').reshape([1, -1, 2])
+    sequence_length = actions.shape[1]
+    states = np.zeros([1, sequence_length, 2], np.float32)
+    image_bytes = np.zeros([1, sequence_length, 64, 64, 3], np.float32)
+    for time_step_idx in range(context_frames):
+        states[0, time_step_idx] = np.fromstring(args.states[time_step_idx], dtype=np.float32, sep=',')
+        rgba_image_uint8 = np.array(Image.open(args.images[time_step_idx]), dtype=np.uint8)
+        rgb_image_float = rgba_image_uint8[:, :, :3].astype(np.float32) / 255.0
+        image_bytes[0, time_step_idx] = rgb_image_float
+    dataset = tf.data.Dataset.from_tensor_slices((OrderedDict({
+        'images': image_bytes,
+        'states': states,
+        'actions': actions,
+    })))
+    batched_dataset = dataset.batch(1, drop_remainder=True)
+    iterator = batched_dataset.make_one_shot_iterator()
+    inputs = iterator.get_next()
+    input_phs = {k: tf.placeholder(v.dtype, v.shape, '%s_ph' % k) for k, v in inputs.items()}
+    return input_phs, inputs, sequence_length
+
+
+def visualize(input_results, results, context_frames, args):
+    # first dimension is batch size, second is time
+    context_images = (input_results['images'][0, :context_frames] * 255.0).astype(np.uint8)
+    context_states = input_results['states'][0, :context_frames]
+    gen_images = (results['images'][0, context_frames:] * 255.0).astype(np.uint8)
+    gen_states = results['states'][0, context_frames:]
+
+    plt.scatter(context_states[:, 0], context_states[:, 1], label='context_states')
+    plt.scatter(gen_states[:, 0], gen_states[:, 1], label='gen_states')
     plt.axis("equal")
 
-    # only keep the future frames
-    gen_images = gen_images[0, -future_length:]
-    context_images_ = (input_results['images'][0] * 255.0).astype(np.uint8)
-    gen_images = (gen_images * 255.0).astype(np.uint8)
-
     # Save gif of input images
-    input_images_fname = 'input_image.gif'
-    save_gif(os.path.join(args.results_dir, input_images_fname), context_images_, fps=args.fps)
+    input_images_filename = 'input_image.gif'
+    save_gif(os.path.join(args.results_dir, input_images_filename), context_images, fps=args.fps)
 
     # Save gif of generated images
-    gen_images_fname = 'gen_image.gif'
-    context_and_gen_images = list(context_images_[:context_frames]) + list(gen_images)
-    save_gif(os.path.join(args.results_dir, gen_images_fname),
-             context_and_gen_images, fps=args.fps)
+    gen_images_filename = 'gen_image.gif'
+    context_and_gen_images = np.concatenate((context_images, gen_images), axis=0)
+    save_gif(os.path.join(args.results_dir, gen_images_filename), context_and_gen_images, fps=args.fps)
 
     # Save gif of errors over time
-    error_images_fname = 'error_image.gif'
-    error_images = context_images_ - context_and_gen_images
-    save_gif(os.path.join(args.results_dir, error_images_fname), error_images, fps=args.fps)
+    error_images_filename = 'error_image.gif'
+    error_images = context_images - context_and_gen_images
+    save_gif(os.path.join(args.results_dir, error_images_filename), error_images, fps=args.fps)
 
-    # Save individual images
-    gen_image_fname_pattern = 'gen_image_%%05d_%%0%dd.png' % max(2, len(str(len(gen_images) - 1)))
+    # Save individual frames of the prediction
+    gen_image_filename_pattern = 'gen_image_%%05d_%%0%dd.png' % max(2, len(str(len(gen_images) - 1)))
     for time_step_idx, gen_image in enumerate(gen_images):
-        gen_image_fname = gen_image_fname_pattern % (time_step_idx, time_step_idx)
-        if gen_image.shape[-1] == 1:
-            gen_image = np.tile(gen_image, (1, 1, 3))
-        else:
-            gen_image = cv2.cvtColor(gen_image, cv2.COLOR_RGB2BGR)
-        cv2.imwrite(os.path.join(args.results_dir, gen_image_fname), gen_image)
+        gen_image_filename = gen_image_filename_pattern % (time_step_idx, time_step_idx)
+        plt.imsave(gen_image_filename, gen_image)
 
     states_plot_filename = os.path.join(args.results_dir, "states_plot.png")
     plt.savefig(fname=states_plot_filename)
     plt.show()
+
 
 if __name__ == '__main__':
     main()
